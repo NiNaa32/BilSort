9b428add061020b4ef3b7570b32628dba2fb41a0
NiNaa32 <112417273+NiNaa32@users.noreply.github.com>
2022-09-17 19:29:47 +0200

    Add project files.

---
+++ Bil.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilSort
+{
+    public class Bil : IComparable
+    {
+        private string tillverkare;
+        private int år;
+
+        public Bil(string tillverkare, int år)
+        {
+            this.tillverkare = tillverkare;
+            this.år = år;
+        }
+        
+        public int År
+        {
+            get { return år; }
+            set { år = value; }
+        }
+        public string Tillverkare
+        {
+            get { return tillverkare; }
+            set { tillverkare = value; }
+        }
+        // Början av kapslade (nested) klasser.
+        // En klass som sorterar efter år i stigande ordning.
+        private class SortÅrStigande : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                Bil c1 = (Bil)a;
+                Bil c2 = (Bil)b;
+                if (c1.år > c2.år)
+                    return 1;
+                if (c1.år < c2.år)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+        // En klass som sorterar efter år i fallande ordning.
+        private class SortÅrFallande : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                Bil c1 = (Bil)a;
+                Bil c2 = (Bil)b;
+                if (c1.år < c2.år)
+                    return 1;
+                if (c1.år > c2.år)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+        // En klass som sorterar efter tillverkare i fallande ordning.
+        private class SortTillverkareFallande : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                Bil c1 = (Bil)a;
+                Bil c2 = (Bil)b;
+                return String.Compare(c2.tillverkare, c1.tillverkare);
+            }
+        }
+        // Implementering av IComparable CompareTo för att sortera efter tillverkare (stigande).
+        int IComparable.CompareTo(object obj)
+        {
+            Bil c = (Bil)obj;
+            return String.Compare(this.tillverkare, c.tillverkare);
+        }
+        // En metod som returnerar ett IComparer-objekt för sortering.
+        public static IComparer sortYearAscending()
+        {
+            return new SortÅrStigande();
+        }
+        // En metod som returnerar ett IComparer-objekt för sortering.
+        public static IComparer sortYearDescending()
+        {
+            return new SortÅrFallande();
+        }
+        // En metod som returnerar ett IComparer-objekt för sortering.
+        public static IComparer sortMakeDescending()
+        {
+            return new SortTillverkareFallande();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace BilSort
+{
+    class Program
+    {
+        static void InmätningBilar(Bil c)
+        {
+            Console.WriteLine("Ange bilinformation:");
+            Console.Write("Tillverkare: ");
+            c.Tillverkare = Console.ReadLine();
+            Console.Write("Tillverkningsår: ");
+            c.År = int.Parse(Console.ReadLine());
+        }
+        static void UtskriftBilar(Bil[] arrayBilar)
+        {
+            foreach (Bil c in arrayBilar)
+            {
+                Console.WriteLine(c.Tillverkare + "\t\t" + c.År);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int i, n;
+            Console.WriteLine("Ange antal bilar\n");
+            n = int.Parse(Console.ReadLine());
+            // Skapa objekt array av klassen Bil..
+            Bil[] arrayBilar = new Bil[n];
+            for (i = 0; i < n; i++)
+            {
+                arrayBilar[i] = new Bil("", 0);
+                InmätningBilar(arrayBilar[i]);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("En osorterad array\n");
+            UtskriftBilar(arrayBilar);
+            // Sortering i stigande ordning efter tillverkare
+            Array.Sort(arrayBilar);
+            Console.WriteLine("\nArray sorterad efter tillverkare (stigande ordning)\n");
+            UtskriftBilar(arrayBilar);
+            //Sortering i stigande ordning efter tillverkningsår med IComparer.
+            Array.Sort(arrayBilar, Bil.sortYearAscending());
+            Console.WriteLine("\nArray sorterad efter tillverkningsår (stigande ordning)\n");
+            UtskriftBilar(arrayBilar);
+            //Sortering i fallande ordning efter tillverkare med IComparer.
+            Console.WriteLine("\nArray sorterad efter tillverkare (fallande ordning)\n");
+            Array.Sort(arrayBilar, Bil.sortMakeDescending());
+            UtskriftBilar(arrayBilar);
+            //Sortering i fallande ordning efter tillverkningsår med IComparer.
+            Array.Sort(arrayBilar, Bil.sortYearDescending());
+            Console.WriteLine("\nArray sorterad efter tillverkningsår (fallande ordning)\n");
+            UtskriftBilar(arrayBilar);
+            Console.ReadKey();
+        }
+    }
+}
